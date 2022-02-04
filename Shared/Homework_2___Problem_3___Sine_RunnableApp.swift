8be42f12190a449ae195e2e284ffdda468e0bef7
Matthew Malaker <MMalaker@hawk.iit.edu>
2022-02-03 21:03:14 -0600

    Initial Commit

---
+++ Shared/Homework_2___Problem_3___Sine_RunnableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_2___Problem_3___Sine_RunnableApp.swift
+//  Shared
+//
+//  Created by Matthew Malaker on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_2___Problem_3___Sine_RunnableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
